2e1f474860e339f89150c035afa654620caff2f3
soroushkavousi <soroushkavousi.me@gmail.com>
2024-01-14 20:47:06 +0330

    refactor(dot-net-core): redesign send sample classes

--- IPE.SmsIrSamples.DotNetCore/Sync/SendSamples.cs
+++ IPE.SmsIrSamples.DotNetCore/Sync/SendSamples.cs
@@ -1,66 +1,70 @@
 ﻿using IPE.SmsIrClient;
+using IPE.SmsIrClient.Models.Requests;
 using System;
-using System.Threading.Tasks;
 
-namespace IPE.SmsIrSamples.DotNetCore.SendSamples;
+namespace IPE.SmsIrSamples.DotNetCore.Sync;
 
-public static class BulkSendSample
+public static class SendSamples
 {
     /// <summary>
-    /// نمونه ارسال گروهی
-    /// https://app.sms.ir/developer/help/bulk
+    /// نمونه ارسال وریفای سینک
+    /// https://app.sms.ir/developer/help/verify
     /// </summary>
-    public static async Task SendSampleBulkAsync()
+    public static void SendSampleVerify()
     {
         try
         {
             // کلید ای‌پی‌آی ساخته‌شده در سامانه
             SmsIr smsIr = new SmsIr("uw7ppC4vGibwGFgAwLyRexHjyEb82yFFEXbbwOoOVT9GVMAQXoDO1vTkx59cOgoJ");
 
-            // شماره خط ارسالی - موجود در قسمت شماره‌های من
-            long lineNumber = 95007079000006;
+            // شماره موبایل مقصد
+            string mobile = "9120000000";
 
-            // متن ارسال گروهی
-            string messageText = "سرویس پیامکی ایده پردازان با بیش از یک دهه سابقه همراه شماست" +
-                "\nSMS.ir";
+            /// <summary>
+            /// شناسه قالب ساخته‌شده در سامانه
+            /// https://app.sms.ir/developer/fast-send/template
+            /// متن قالب نمونه:
+            /// جناب #NAME#
+            /// کد ورود:
+            /// Code:#CODE#
+            /// سامانه پیامکی SMS.ir
+            /// </summary>
+            int templateId = 200000;
 
-            // شماره موبایل‌های مقصد
-            string[] mobiles = { "9120000000", "9120000001" };
+            // مقداردهی پارامترهای استفاده شده در قالب
+            string name = "آقای محمد محمدی";
+            int code = 12345;
+            VerifySendParameter[] verifySendParameters = {
+                new VerifySendParameter("NAME", name),
+                new VerifySendParameter("CODE", code.ToString()),
+            };
 
-            // پارامتر اختیاری زمان ارسال
-            // زمان ارسال به صورت یونیکس تایم می‌باشد مثلا 1704094200
-            int? sendDateTime = null;
-
-            // انجام ارسال گروهی
-            var sendResult = await smsIr.BulkSendAsync(lineNumber, messageText, mobiles, sendDateTime);
+            // انجام ارسال وریفای
+            var sendResult = smsIr.VerifySend(mobile, templateId, verifySendParameters);
 
             // ارسال شما در اینجا با موفقیت انجام شده‌است.
 
-            //شناسه دسته پیامکی ارسال شده
-            Guid packId = sendResult.Data.PackId;
-
-            //لیست شناسه پیامک‌های ارسال شده
-            int?[] messageIds = sendResult.Data.MessageIds;
+            //شناسه پیامک ارسال شده
+            int messageId = sendResult.Data.MessageId;
 
             //هزینه ارسال
             decimal cost = sendResult.Data.Cost;
 
             string resultDescription = "Your message was sent successfully." +
-                $"\n - Pack Id: {packId} " +
-                $"\n - Message Ids: [{string.Join(", ", messageIds)}] " +
+                $"\n - Message Id: {messageId} " +
                 $"\n - Cost: {cost}";
 
-            await Console.Out.WriteLineAsync(resultDescription);
+            Console.Out.WriteLine(resultDescription);
         }
         catch (Exception ex) // ارسال ناموفق
         {
             // جدول توضیحات کد وضعیت
             // https://app.sms.ir/developer/help/statusCode
 
-            var errorDescription = "Unable to send SMS message." +
+            string errorDescription = "Unable to send SMS message." +
                 $"\n - Error: {ex.Message}";
 
-            await Console.Out.WriteLineAsync(errorDescription);
+            Console.Out.WriteLine(errorDescription);
         }
     }
 
@@ -91,6 +95,69 @@ public static class BulkSendSample
 
             // انجام ارسال گروهی
             var sendResult = smsIr.BulkSend(lineNumber, messageText, mobiles, sendDateTime);
+
+            // ارسال شما در اینجا با موفقیت انجام شده‌است.
+
+            //شناسه دسته پیامکی ارسال شده
+            Guid packId = sendResult.Data.PackId;
+
+            //لیست شناسه پیامک‌های ارسال شده
+            int?[] messageIds = sendResult.Data.MessageIds;
+
+            //هزینه ارسال
+            decimal cost = sendResult.Data.Cost;
+
+            string resultDescription = "Your message was sent successfully." +
+                $"\n - Pack Id: {packId} " +
+                $"\n - Message Ids: [{string.Join(", ", messageIds)}] " +
+                $"\n - Cost: {cost}";
+
+            Console.Out.WriteLine(resultDescription);
+        }
+        catch (Exception ex) // ارسال ناموفق
+        {
+            // جدول توضیحات کد وضعیت
+            // https://app.sms.ir/developer/help/statusCode
+
+            var errorDescription = "Unable to send SMS message." +
+                $"\n - Error: {ex.Message}";
+
+            Console.Out.WriteLine(errorDescription);
+        }
+    }
+
+    /// <summary>
+    /// نمونه ارسال نظیر به نظیر سینک
+    /// https://app.sms.ir/developer/help/likeToLike
+    /// </summary>
+    public static void SendSampleLikeToLike()
+    {
+        try
+        {
+            // کلید ای‌پی‌آی ساخته‌شده در سامانه
+            SmsIr smsIr = new SmsIr("uw7ppC4vGibwGFgAwLyRexHjyEb82yFFEXbbwOoOVT9GVMAQXoDO1vTkx59cOgoJ");
+
+            // شماره خط ارسالی - موجود در قسمت شماره‌های من
+            long lineNumber = 95007079000006;
+
+            // متن‌های ارسال نظیر به نظیر
+            string[] messageTexts =
+            {
+                "سرویس پیامکی ایده پردازان با بیش از یک دهه سابقه همراه شماست" +
+                "\nSMS.ir",
+                "پلتفرم آموزش آنلاین، آکادمی ایده پردازان" +
+                "\nipdemy.ir"
+            };
+
+            // شماره موبایل‌های مقصد
+            string[] mobiles = { "9120000000", "9120000001" };
+
+            // پارامتر اختیاری زمان ارسال
+            // زمان ارسال به صورت یونیکس تایم می‌باشد مثلا 1704094200
+            int? sendDateTime = null;
+
+            // انجام ارسال نظیر به نظیر
+            var sendResult = smsIr.LikeToLikeSend(lineNumber, messageTexts, mobiles, sendDateTime);
 
             // ارسال شما در اینجا با موفقیت انجام شده‌است.
 

dad5a899e14c52adebcd97b88255a303f5a08773
SoroushKavousi <soroushkavousi.me@gmail.com>
2024-01-16 09:18:38 +0330

    feat(receive): add receive samples

--- IPE.SmsIrSamples.DotNetCore/Program.cs
+++ IPE.SmsIrSamples.DotNetCore/Program.cs
@@ -1,25 +1,34 @@
-﻿using IPE.SmsIrSamples.DotNetCore.Report.Send;
+﻿using IPE.SmsIrSamples.DotNetCore.Report.Receive;
+using IPE.SmsIrSamples.DotNetCore.Report.Send;
 using IPE.SmsIrSamples.DotNetCore.Send;
 using System;
 
 try
 {
-    // sends
+    ///////////////// sends
+
     //await SendVerifySamples.SendVerifyAsync();
     //await SendBulkSamples.SendBulkAsync();
     //await SendLikeToLikeSamples.SendLikeToLikeAsync();
 
-    // get send reports
+    ///////////////// send reports
 
+    ////// get send reports
     //await GetSendLiveReportSamples.GetSendLiveReportAsync();
     //await GetSendArchiveReportSamples.GetSendArchiveReportAsync();
 
-    // get single message
+    ////// get single message
     //await GetSingleMessageSamples.GetSingleMessageReportAsync();
 
-    // get send packs
+    ////// get send packs
     //await GetSendPackSamples.GetSendPackLiveReportAsync();
     //await GetSendPacksSamples.GetSendPacksLiveReportAsync();
+
+    ///////////////// receive reports
+
+    //await GetLatestReceivesSamples.GetLatestReceivesAsync();
+    //await GetReceiveLiveReportSamples.GetReceiveLiveReportAsync();
+    //await GetReceiveArchiveReportSamples.GetReceiveArchiveReportAsync();
 
     await Console.Out.WriteLineAsync("Finished!");
 }
---
+++ IPE.SmsIrSamples.DotNetCore/Report/Receive/GetLatestReceivesSamples.cs
@@ -0,0 +1,88 @@
+﻿using IPE.SmsIrClient;
+using IPE.SmsIrClient.Models.Results;
+using System;
+using System.Threading.Tasks;
+
+namespace IPE.SmsIrSamples.DotNetCore.Report.Receive;
+
+public static class GetLatestReceivesSamples
+{
+    /// <summary>
+    /// گزارش تازه‌ترین پیامک‌های دریافتی
+    /// لازم به ذکر است هر پیامک دریافتی تنها یک مرتبه توسط این متد قابل دستیابی می‌باشد
+    /// و پس از آن به دلیل قرار گرفتن در حالت خوانده شده قابل دسترسی مجدد توسط این متد نمی‌باشند.
+    /// https://app.sms.ir/developer/help/receiveLatest
+    /// </summary>
+    public static async Task GetLatestReceivesAsync()
+    {
+        try
+        {
+            // کلید ای‌پی‌آی ساخته‌شده در سامانه
+            SmsIr smsIr = new SmsIr("uw7ppC4vGibwGFgAwLyRexHjyEb82yFFEXbbwOoOVT9GVMAQXoDO1vTkx59cOgoJ");
+
+            // گزارش‌ها در صفحات حداکثر 100 تایی قابل دریافت می‌باشد.
+
+            // تعداد درخواستی - دارای پیشفرض 100
+            // این عدد نمی‌تواند بیشتر از 100 باشد.
+            int count = 100;
+
+            // انجام دریافت گزارش
+            // مرتب سازی براساس قدیمی‌ترین دریافت می‌باشد.
+            var response = await smsIr.GetLatestReceivesAsync(count);
+
+            // دریافت گزارش شما در اینجا با موفقیت انجام شده‌است.
+
+            // گرفتن بخش دیتا خروجی
+            ReceivedMessageResult[] messages = response.Data;
+
+            // بررسی و نمایش پیامک‌های گزارش
+            string reportDescription = string.Empty;
+            foreach (var message in messages)
+            {
+                // شماره موبایل فرستنده
+                long mobile = message.Mobile;
+
+                // خط دریافت کننده برای ارسال
+                long lineNumber = message.Number;
+
+                // متن پیام فرستاده شده
+                string messageText = message.MessageText;
+
+                // زمان دریافت
+                int receivedUnixTime = message.ReceivedDateTime;
+                DateTime receivedDateTime = DateTimeOffset.FromUnixTimeSeconds(receivedUnixTime).DateTime.ToLocalTime();
+
+                // خلاصه‌ای از هر دریافت
+                string messageDescription = $"Mobile: {mobile}" +
+                    $" - Line Number: {lineNumber}" +
+                    $" - Received DateTime: {receivedDateTime}" +
+                    $" - Message Text: {messageText}" +
+                    Environment.NewLine;
+
+                reportDescription += messageDescription;
+            }
+
+            await Console.Out.WriteLineAsync(reportDescription);
+        }
+        catch (Exception ex) // درخواست ناموفق
+        {
+            // جدول توضیحات کد وضعیت
+            // https://app.sms.ir/developer/help/statusCode
+
+            string errorName = ex.GetType().Name;
+            string errorNameDescription = errorName switch
+            {
+                "UnauthorizedException" => "Token is not valid or access denied",
+                "LogicalException" => "Please fix the request parameters",
+                "TooManyRequestException" => "Request count has exceed the limitation",
+                "UnexpectedException" or "InvalidOperationException" => "Remote server error",
+                _ => "Can not send the request",
+            };
+
+            var errorDescription = "There is a problem with the request." +
+                $"\n - Error: {errorName} - {errorNameDescription} - {ex.Message}";
+
+            await Console.Out.WriteLineAsync(errorDescription);
+        }
+    }
+}
